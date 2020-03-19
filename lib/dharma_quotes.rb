afc8ba4672e8b3cc9c0be17ae3554c3f7319d32d
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-19 22:52:39 +0000

    Added two more methods and two more arrays

--- lib/dharma_quotes.rb
+++ lib/dharma_quotes.rb
@@ -19,10 +19,38 @@ class DharmaQuotes
             happiness follows,
             As surely as one\'s shadow.'
         ]
+        @parts_of_eightfold_path = 
+        [
+            'Samma Ditthi: right understanding',
+            'Samma Sankappa: right thought',
+            'Samma Vaca: right speech',
+            'Samma Kammanta: right action',
+            'Samma Ajiva: right livelihood',
+            'Samma Vayama: right effort',
+            'Samma Sati: right mindfulness',
+            'Samma Samadhi: right concentration'
+        ]
+        @four_noble_truths = 
+        [
+            'To live is to suffer.',
+            'There is a cause to our suffering, and that is desire.',
+            'We can end this suffering.',
+            'The way to end our suffering is by following the Noble Eightfold Path.'
+        ]
     end
 
     # pick a random quote
     def get_quote
         @quotes.sample
+    end
+
+    # pick one of the parts of the eightfold path
+    def get_part_of_eightfold_path
+        @parts_of_eightfold_path.sample
+    end
+
+    # pick one of the four noble truths
+    def get_noble_truth
+        @four_noble_truths.sample
     end
 end
