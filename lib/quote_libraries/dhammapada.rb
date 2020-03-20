faede094d240e44f7d09f17c0cc6e75fe20bec55
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-20 11:21:57 +0000

    Removed all quote arrays from quote generator class and stored them instead in separate classes, which I require in generator class

---
+++ lib/quote_libraries/dhammapada.rb
@@ -0,0 +1,26 @@
+class Dhammapada
+   def initialize
+    @dhammapada_quotes = 
+    [
+        '
+        Animosity does not eradicate animosity. 
+        Only by loving kindness is animosity dissolved. 
+        This law is ancient and eternal. - The Dhammapada',
+        '
+        Mind is the forerunner of all actions.
+        All deeds are led by mind, created by mind.
+        If one speaks or acts with corrupt mind, suffering follows,
+        As the wheel follows the hoof of an ox pulling a cart. - The Dhammapada',
+        '
+        Mind is the forerunner of all actions.
+        All deeds are led by mind, created by mind.
+        If one speaks or acts with a serene mind, happiness follows,
+        As surely as one\'s shadow. - The Dhammapada'
+    ]
+   end
+
+   # return the @dhammapada_quotes array stored in the instance var
+   def quotes
+       @dhammapada_quotes
+   end
+end
---
+++ lib/quote_libraries/other_texts.rb
@@ -0,0 +1,33 @@
+class OtherTexts
+    def initialize
+        @quotes = 
+        [
+            'On whom the heart insinctively rests,
+            In whom the spirit finds delight,
+            With him, though one ne\'er seen before,
+            Safely in friendship one may dwell. - The Jataka
+            ',
+            '
+            This is what should be done by one who is skilled in goodness,
+            And who knows the path of of peace: let them be able and upright,
+            Straightforward and gentle in speech,
+            Humble and not conceited,
+            Contented and easily satisfied,
+            Unburdened with duties and frugal in their ways. - Morning and Evening Puja and Reflections
+            ',
+            '
+            Let none through anger or ill will wish harm upon another.
+            Even as a mother protects with her life her child, her only child,
+            So with a boundless heart
+            Should one cherish all living beings;
+            Radiating kindness over the entire world. - Morning and Evening Puja and Reflections
+            ',
+            '
+            By not holding to fixed views,
+            The pure-hearted one, having clarity of vision,
+            Being freed from all sense-desires,
+            Is not born again in this world. - Morning and Evening Puja and Reflections
+            '
+        ]
+    end
+end
