695b0f43e058bf955d8e9340dcd5b6324be032e6
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-20 12:07:44 +0000

    Added more Dhammapada quotes

--- lib/quote_libraries/dhammapada.rb
+++ lib/quote_libraries/dhammapada.rb
@@ -19,8 +19,44 @@ class Dhammapada
         '
         By oneself the evil is done, by oneself one suffers.
         By oneself evil is left undone, by oneself one is purified.
-        The pure and the impure (stand and fall) by themselves: no one can purify another.
+        The pure and the impure (stand and fall) by themselves: no one can purify another. - The Dhammapada
         ',
+        '
+        The wise one makes straight
+        The trembling, fickle mind
+        So hard to guard, so hard to control,
+        As the fletcher makes straight the arrow. - The Dhammapada
+        ',
+        '
+        The mind is hard to restrain, light,
+        Flying where it will.
+        Control of it is good.
+        Mind controlled brings happiness. - The Dhammapada
+        ',
+        '
+        Whatever an enemy can do to an enemy,
+        Or a rival to a rival,
+        A wrongly directed mind
+        Will do worse to you than that. - The Dhammapada
+        ',
+        '
+        What mother or father can do,
+        Or any other kin,
+        A rightly directed mind
+        Will do better for you than that. - The Dhammapada
+        ',
+        '
+        The Learner will conquer this earth,
+        And this world of Yama with its gods.
+        The Learner will pluck the well-taught word of Dhamma
+        As a skilled one plucks a flower. - The Dhammapada
+        ',
+        '
+        Like a beautiful flower,
+        Colourful but scentless,
+        The well-taught word is fruitless
+        For one who does not practise. - The Dhammapada
+        '
     ]
    end
 
