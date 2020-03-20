41d616f715fed2d447cd799f6da72257c1138fde
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-20 11:01:54 +0000

    Added more rb files because want to separate the different quote sources to keep the volume of quotes manageable

--- lib/dharma_quotes.rb
+++ lib/dharma_quotes.rb
@@ -5,19 +5,45 @@ class DharmaQuotes
             '
             Animosity does not eradicate animosity. 
             Only by loving kindness is animosity dissolved. 
-            This law is ancient and eternal.',
-            '
-            Mind is a forerunner of all actions.
-            All deeds are led by mind, created by mind.
-            If one speaks or acts with corrupt mind,
-            suffering follows,
-            As the wheel follows the hoof of an ox pulling a cart.',
+            This law is ancient and eternal. - The Dhammapada',
             '
             Mind is the forerunner of all actions.
             All deeds are led by mind, created by mind.
-            If one speaks or acts with a serene mind,
-            happiness follows,
-            As surely as one\'s shadow.'
+            If one speaks or acts with corrupt mind, suffering follows,
+            As the wheel follows the hoof of an ox pulling a cart. - The Dhammapada',
+            '
+            Mind is the forerunner of all actions.
+            All deeds are led by mind, created by mind.
+            If one speaks or acts with a serene mind, happiness follows,
+            As surely as one\'s shadow. - The Dhammapada',
+            '
+            On whom the heart insinctively rests,
+            In whom the spirit finds delight,
+            With him, though one ne\'er seen before,
+            Safely in friendship one may dwell. - The Jataka
+            ',
+            '
+            This is what should be done by one who is skilled in goodness,
+            And who knows the path of of peace: let them be able and upright,
+            Straightforward and gentle in speech,
+            Humble and not conceited,
+            Contented and easily satisfied,
+            Unburdened with duties and frugal in their ways. - Morning and Evening Puja and Reflections
+            ',
+            '
+            Let none through anger or ill will wish harm upon another.
+            Even as a mother protects with her life her child, her only child,
+            So with a boundless heart
+            Should one cherish all living beings;
+            Radiating kindness over the entire world. - Morning and Evening Puja and Reflections
+            ',
+            '
+            By not holding to fixed views,
+            The pure-hearted one, having clarity of vision,
+            Being freed from all sense-desires,
+            Is not born again in this world. - Morning and Evening Puja and Reflections
+            ',
+            
         ]
         @parts_of_eightfold_path = 
         [
