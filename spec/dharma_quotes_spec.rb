93a6417def770aa23e216d8d71e062032fc4f537
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-20 12:34:48 +0000

    Started writing tests for generator

---
+++ spec/dharma_quotes_spec.rb
@@ -0,0 +1,9 @@
+require 'dharma_quotes'
+
+dharma_quotes = DharmaQuotes.new
+
+RSpec.describe dharma_quotes do
+    describe '#get_quote' do
+        it 'returns a random quote from either the Dhammapada or another text'
+    end
+end
