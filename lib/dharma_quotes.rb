4032781d55b2e81c1531427c5a0f25f5a8b03030
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-06-17 15:34:46 +0100

    Updated version no in readme and gemspec. Removed redundant comments

--- lib/dharma_quotes.rb
+++ lib/dharma_quotes.rb
@@ -12,28 +12,23 @@ class DharmaQuotes
         @parts_of_eightfold_path = NobleEightfoldPath.new.quotes    
     end
 
-    # pick a random quote
     def get_quote
         dhammapada_and_other_texts_quotes = @dhammapada_quotes.concat(@other_texts_quotes)
         dhammapada_and_other_texts_quotes.sample
     end
 
-    # pick a random dhammapada quote
     def get_dhammapada_quote
         @dhammapada_quotes.sample
     end
 
-    # pick a random quote from other texts
     def get_quote_from_other_texts
         @other_texts_quotes.sample
     end
 
-    # pick one of the parts of the eightfold path
     def get_part_of_eightfold_path
         @parts_of_eightfold_path.sample
     end
 
-    # pick one of the four noble truths
     def get_noble_truth
         @four_noble_truths.sample
     end
